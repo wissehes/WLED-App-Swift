66ac3394b7ec463919ec3b90cb7e7158bbfeac67
Wisse <wissehes@gmail.com>
2021-07-08 22:47:35 +0200

    Initial Commit

---
+++ WLED Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WLED Swift
+//
+//  Created by Wisse Hes on 08/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WLED Swift/WLED_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WLED_SwiftApp.swift
+//  WLED Swift
+//
+//  Created by Wisse Hes on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct WLED_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
