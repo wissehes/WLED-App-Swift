e5be815516ae0551bfe74c8af4e8d0a909d719e9
Wisse <wissehes@gmail.com>
2021-09-26 20:50:45 +0200

    Add presetIndex, playlistIndex and udpn send/receive to WledApiResponse

--- WLED Swift/Shared/Structs/WledApiResponse.swift
+++ WLED Swift/Shared/Structs/WledApiResponse.swift
@@ -14,10 +14,26 @@ struct WledApiResponse: Codable {
 struct WledState: Codable {
     let on: Bool
     let brightness: Float
+    let presetIndex: Int
+    let playlistIndex: Int
+    let udpn: WledUDPN
     
     
     enum CodingKeys: String, CodingKey {
-        case on         = "on"
-        case brightness = "bri"
+        case on            = "on"
+        case brightness    = "bri"
+        case presetIndex   = "ps"
+        case playlistIndex = "pl"
+        case udpn          = "udpn"
+    }
+}
+
+struct WledUDPN: Codable {
+    let send: Bool
+    let receive: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case send
+        case receive = "recv"
     }
 }
